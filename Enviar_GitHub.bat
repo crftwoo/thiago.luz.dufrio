7a54f9087044a98e08e4b45eff2e17640b1a2cb9
Thiago Luz <thiago.luz@dufrio.com.br>
2026-04-01 22:09:22 -0300

    feat: Adicionada nova ferramenta e ajustes

---
+++ Enviar_GitHub.bat
@@ -0,0 +1,25 @@
+@echo off
+echo ===================================================
+echo     SALVANDO ALTERACOES NO GITHUB
+echo ===================================================
+echo.
+
+echo 1. Adicionando arquivos modificados...
+git add .
+echo.
+
+echo 2. Salvando as versoes...
+git config user.name "Thiago Luz"
+git config user.email "thiago.luz@dufrio.com.br"
+git commit -m "feat: Adicionada nova ferramenta e ajustes"
+echo.
+
+echo 3. Enviando para a nuvem...
+git push origin main
+echo.
+
+echo ===================================================
+echo     TUDO PRONTO! SEUS ARQUIVOS ESTAO NO GITHUB!
+echo ===================================================
+timeout /t 3 >nul
+exit
