07c985460ed319defb6b8f68a535405e89699c4e
Thiago Luz <thiago.luz@dufrio.com.br>
2026-04-10 14:44:21 -0300

    teste

--- Enviar_GitHub.bat
+++ Enviar_GitHub.bat
@@ -1,9 +1,14 @@
 @echo off
+setlocal EnableDelayedExpansion
 echo ===================================================
 echo     SALVANDO ALTERACOES NO GITHUB
 echo ===================================================
 echo.
 
+set /p userMsg="O que voce alterou hoje? (ou de um Enter para pular): "
+if "!userMsg!"=="" set userMsg=Atualizacao de Rotina do Sistema
+
+echo.
 echo 1. Adicionando arquivos modificados...
 git add .
 echo.
@@ -11,7 +16,7 @@ echo.
 echo 2. Salvando as versoes...
 git config user.name "Thiago Luz"
 git config user.email "thiago.luz@dufrio.com.br"
-git commit -F mensagem_atualizacao.txt
+git commit -m "!userMsg!"
 echo.
 
 echo 3. Enviando para a nuvem...
@@ -21,5 +26,5 @@ echo.
 echo ===================================================
 echo     TUDO PRONTO! SEUS ARQUIVOS ESTAO NO GITHUB!
 echo ===================================================
-timeout /t 3 >nul
+timeout /t 5 >nul
 exit
