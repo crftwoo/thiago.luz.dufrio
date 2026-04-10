4fe633f1d3ac6d7893af7a69d6e3304003df3b89
Thiago Luz <thiago.luz@dufrio.com.br>
2026-04-10 14:47:25 -0300

    Novo Design: Adicionado visual Bento Grid (Neon Dark Mode) e finalizada as rotas do Plano de Corte.

--- Enviar_GitHub.bat
+++ Enviar_GitHub.bat
@@ -1,14 +1,9 @@
 @echo off
-setlocal EnableDelayedExpansion
 echo ===================================================
 echo     SALVANDO ALTERACOES NO GITHUB
 echo ===================================================
 echo.
 
-set /p userMsg="O que voce alterou hoje? (ou de um Enter para pular): "
-if "!userMsg!"=="" set userMsg=Atualizacao de Rotina do Sistema
-
-echo.
 echo 1. Adicionando arquivos modificados...
 git add .
 echo.
@@ -16,7 +11,7 @@ echo.
 echo 2. Salvando as versoes...
 git config user.name "Thiago Luz"
 git config user.email "thiago.luz@dufrio.com.br"
-git commit -m "!userMsg!"
+git commit -F mensagem_atualizacao.txt
 echo.
 
 echo 3. Enviando para a nuvem...
@@ -26,5 +21,5 @@ echo.
 echo ===================================================
 echo     TUDO PRONTO! SEUS ARQUIVOS ESTAO NO GITHUB!
 echo ===================================================
-timeout /t 5 >nul
+timeout /t 3 >nul
 exit
