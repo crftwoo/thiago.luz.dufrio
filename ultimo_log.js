108eb2f543869d6ce7547e73a10ced1bff8ddcc6
Thiago Luz <thiago.luz@dufrio.com.br>
2026-04-16 10:10:31 -0300

    UX Mobile: Layout 100% responsivo, tabelas com scroll lateral, adequação de escala no 3D e botões empilhados (Touch).

--- ultimo_log.js
+++ ultimo_log.js
@@ -1,1 +1,1 @@
-const LATEST_LOG = { message: 'UX Mobile: Layout 100% responsivo, tabelas com scroll lateral, adequação de escala no 3D e botões empilhados (Touch).', dateIso: '2026-04-16T09:38:46-03:00' };
+const LATEST_LOG = { message: 'UX Mobile: Layout 100% responsivo, tabelas com scroll lateral, adequação de escala no 3D e botões empilhados (Touch).', dateIso: '2026-04-16T10:10:31-03:00' };
