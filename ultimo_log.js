184a1aaada56f99c6d5dbecd1100e105c48664f5
Thiago Luz <thiago.luz@dufrio.com.br>
2026-04-22 16:35:05 -0300

    Scraper-ar: redesign completo dos cards com foto, precos, botao link e copia igual a extensao

--- ultimo_log.js
+++ ultimo_log.js
@@ -1,1 +1,1 @@
-const LATEST_LOG = { message: 'Scraper-ar: redesign completo dos cards com foto, precos, botao link e copia igual a extensao', dateIso: '2026-04-22T16:07:34-03:00' };
+const LATEST_LOG = { message: 'Scraper-ar: redesign completo dos cards com foto, precos, botao link e copia igual a extensao', dateIso: '2026-04-22T16:35:05-03:00' };
