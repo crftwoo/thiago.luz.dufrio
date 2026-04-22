aa2bc0088b4520d533b2f67a9e57c1b4a4a32caf
Thiago Luz <thiago.luz@dufrio.com.br>
2026-04-22 16:07:34 -0300

    Scraper-ar: redesign completo dos cards com foto, precos, botao link e copia igual a extensao

--- ultimo_log.js
+++ ultimo_log.js
@@ -1,1 +1,1 @@
-const LATEST_LOG = { message: 'Scraper Dufrio: corrigido link dos produtos (adiciona .html ao url_key)', dateIso: '2026-04-22T16:01:41-03:00' };
+const LATEST_LOG = { message: 'Scraper-ar: redesign completo dos cards com foto, precos, botao link e copia igual a extensao', dateIso: '2026-04-22T16:07:34-03:00' };
