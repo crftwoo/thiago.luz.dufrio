6619fd044c9137b8c548b3e4e72d28b85dca3988
Thiago Luz <thiago.luz@dufrio.com.br>
2026-04-22 22:50:20 -0300

    Adicionado sistema de filtros dinâmicos de marcas, btus e ciclos no scraper, além de botão para refazer a busca.

--- ultimo_log.js
+++ ultimo_log.js
@@ -1,1 +1,1 @@
-const LATEST_LOG = { message: 'Adicionado sistema de filtros dinâmicos de marcas, btus e ciclos no scraper, além de botão para refazer a busca.', dateIso: '2026-04-22T22:42:19-03:00' };
+const LATEST_LOG = { message: 'Adicionado sistema de filtros dinâmicos de marcas, btus e ciclos no scraper, além de botão para refazer a busca.', dateIso: '2026-04-22T22:50:20-03:00' };
