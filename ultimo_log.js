1394f42dad47458e1a1a360fad9f41226599f151
Thiago Luz <thiago.luz@dufrio.com.br>
2026-04-22 23:38:30 -0300

    Adicionado selo "Em Produção" ao card da ferramenta Preços ao Vivo no painel inicial do Hub.

--- ultimo_log.js
+++ ultimo_log.js
@@ -1,1 +1,1 @@
-const LATEST_LOG = { message: 'Limpando arquivos de teste esquecidos', dateIso: '2026-04-22T22:57:40-03:00' };
+const LATEST_LOG = { message: 'Adicionado selo "Em Produção" ao card da ferramenta Preços ao Vivo no painel inicial do Hub.', dateIso: '2026-04-22T23:38:30-03:00' };
