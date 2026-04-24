cc5808ef7ce1e65c4e9a20d95d7c0363ef2148f4
Thiago Luz <thiago.luz@dufrio.com.br>
2026-04-24 12:24:13 -0300

    WebContinental integrada via API Direta (VTEX IO) - Sem necessidade de Proxy.

--- ultimo_log.js
+++ ultimo_log.js
@@ -1,1 +1,1 @@
-const LATEST_LOG = { message: 'WebContinental integrada via API Direta (VTEX IO) - Sem necessidade de Proxy.', dateIso: '2026-04-24T12:18:06-03:00' };
+const LATEST_LOG = { message: 'WebContinental integrada via API Direta (VTEX IO) - Sem necessidade de Proxy.', dateIso: '2026-04-24T12:24:13-03:00' };
