728cc08306ae00c7f7bcf8912ae5d4b1b9079081
Thiago Luz <thiago.luz@dufrio.com.br>
2026-04-28 15:37:12 -0300

    Adicionado scraper da ClimaRio e atualizados logs.

--- ultimo_log.js
+++ ultimo_log.js
@@ -1,1 +1,1 @@
-const LATEST_LOG = { message: 'Re-implementado scraper da Dufrio e atualizados logs.', dateIso: '2026-04-28T15:23:12-03:00' };
+const LATEST_LOG = { message: 'Adicionado scraper da ClimaRio e atualizados logs.', dateIso: '2026-04-28T15:37:12-03:00' };
