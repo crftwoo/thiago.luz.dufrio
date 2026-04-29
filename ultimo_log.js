b445e8e0900f525d7688080b2c1777296b9889e8
Thiago Luz <thiago.luz@dufrio.com.br>
2026-04-29 13:37:26 -0300

    Sucesso: Scraper ClimaRio finalizado com classes oficiais e redundância de proxies.

--- ultimo_log.js
+++ ultimo_log.js
@@ -1,1 +1,1 @@
-const LATEST_LOG = { message: 'Fix: Melhorada a agressividade da extração do window.__STATE__ do VTEX para garantir resultados na ClimaRio.', dateIso: '2026-04-29T12:04:23-03:00' };
+const LATEST_LOG = { message: 'Sucesso: Scraper ClimaRio finalizado com classes oficiais e redundância de proxies.', dateIso: '2026-04-29T13:37:26-03:00' };
